3377a8bca5f63c322f5abc2ce42ed59e42093e7b
chtpx <muhammadhassanamin007@gmail.com>
2019-10-22 23:30:24 +0500

    modified navigation along with menu aka home signup on mainactivity will work as a activity changer signup->home Navigation updated and finalized edit profile,profile also done

---
+++ app/src/main/java/com/tpxsofts/helpdroid/Main3Activityprofile.java
@@ -0,0 +1,13 @@
+package com.tpxsofts.helpdroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activityprofile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3_activityprofile);
+    }
+}
---
+++ app/src/main/java/com/tpxsofts/helpdroid/MainActivityeditprofile.java
@@ -0,0 +1,18 @@
+package com.tpxsofts.helpdroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivityeditprofile extends AppCompatActivity {
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editprofile);
+    }
+
+
+
+
+}
